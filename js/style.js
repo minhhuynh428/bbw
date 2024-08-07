a5db83e561c21e028f7a8527c9c6ab87e2688801
minh <huynhanhminh284@gmail.com>
2024-08-07 17:45:21 +0700

    ver 1.2

--- js/style.js
+++ js/style.js
@@ -69,18 +69,123 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //Tạo sticky cho thanh menu trên điện thoại khi scroll
+// document.addEventListener("DOMContentLoaded", function () {
+//   var header = document.querySelector(".l-head");
+//   var banner = document.getElementById("banner");
+//   var sticky = banner.offsetHeight;
+//   console.log(sticky);
+//   function myFunction() {
+//     if (window.scrollY >= sticky) {
+//       header.classList.add("sticky");
+//     } else {
+//       header.classList.remove("sticky");
+//     }
+//   }
+//   myFunction();
+//   window.addEventListener("scroll", myFunction);
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var didScroll;
+//   var lastScrollTop = 0;
+//   var delta = 5;
+//   var banner = document.querySelector(".l-banner");
+//   bannerHeight = banner.offsetHeight;
+//   var header = document.querySelector(".l-head");
+//   navbarHeight = header.offsetHeight;
+
+//   window.addEventListener("scroll", function () {
+//     didScroll = true;
+//   });
+
+//   setInterval(function () {
+//     if (didScroll) {
+//       hasScrolled();
+//       didScroll = false;
+//     }
+//   }, 100);
+
+//   function hasScrolled() {
+//     var st = window.scrollY;
+
+//     if (Math.abs(lastScrollTop - st) <= delta) {
+//       return;
+//     }
+
+//     if (st > lastScrollTop && st > navbarHeight) {
+//       console.log(1);
+//       header.classList.remove("shadow");
+//       header.classList.remove("adjust");
+//     } else {
+//       if (
+//         st + window.innerHeight < document.documentElement.scrollHeight &&
+//         st > bannerHeight + navbarHeight
+//       ) {
+//         console.log(2);
+//         header.classList.add("adjust");
+//         header.classList.add("shadow");
+//       }
+//       if (st < bannerHeight) {
+//         console.log(4);
+//         header.classList.remove("adjust");
+//         header.classList.remove("shadow");
+//       }
+//     }
+
+//     lastScrollTop = st;
+//   }
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
+  var lastScrollTop = 0;
+  var delta = 5;
+  var banner = document.querySelector(".l-banner");
+  var bannerHeight = banner.offsetHeight;
   var header = document.querySelector(".l-head");
-  var banner = document.getElementById("banner");
-  var sticky = banner.offsetHeight;
-  console.log(sticky);
-  function myFunction() {
-    if (window.scrollY >= sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
+  var navbarHeight = header.offsetHeight;
+  var ticking = false;
+
+  window.addEventListener("scroll", function () {
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        hasScrolled();
+        ticking = false;
+      });
+      ticking = true;
     }
+  });
+  // Yêu cầu requestAnimationFrame: Khi bạn gọi requestAnimationFrame, bạn đang yêu cầu trình duyệt gọi hàm callback của bạn (hàm chứa hasScrolled()) trong lần vẽ lại màn hình tiếp theo. Việc này không diễn ra ngay lập tức mà được lên lịch để thực hiện trước khi trình duyệt thực hiện lần vẽ lại tiếp theo (thường là khoảng 1/60 giây, tương đương với 60 lần mỗi giây nếu màn hình là 60Hz).
+
+  // Đặt ticking = true ngay lập tức: Ngay sau khi yêu cầu requestAnimationFrame, bạn đặt ticking = true. Điều này giúp đảm bảo rằng không có yêu cầu requestAnimationFrame nào khác được gửi trong khi yêu cầu hiện tại vẫn đang chờ. Nếu bạn không làm điều này, có thể xảy ra nhiều sự kiện scroll trong thời gian ngắn và gửi nhiều yêu cầu requestAnimationFrame, dẫn đến việc hàm hasScrolled được gọi nhiều lần không cần thiết.
+
+  // Thực thi hàm callback trong requestAnimationFrame: Khi trình duyệt chuẩn bị vẽ lại màn hình, nó sẽ thực thi hàm callback bạn đã cung cấp cho requestAnimationFrame. Trong hàm callback này, hasScrolled được gọi và ticking được đặt lại false để cho phép các yêu cầu mới sau khi hàm hiện tại hoàn thành.
+  function hasScrolled() {
+    var st = window.scrollY;
+
+    if (Math.abs(lastScrollTop - st) <= delta) {
+      return;
+    }
+    if (st > lastScrollTop && st > navbarHeight) {
+      header.classList.remove("shadow");
+      header.classList.remove("adjust");
+    } else {
+      if (st >= bannerHeight + navbarHeight) {
+        header.classList.add("ontop");
+      }
+      if (
+        st + window.innerHeight < document.documentElement.scrollHeight &&
+        st >= bannerHeight + navbarHeight
+      ) {
+        header.classList.remove("ontop");
+        header.classList.add("adjust");
+        header.classList.add("shadow");
+      }
+      if (st < bannerHeight) {
+        header.classList.remove("adjust");
+        header.classList.remove("shadow");
+      }
+    }
+
+    lastScrollTop = st;
   }
-  myFunction();
-  window.addEventListener("scroll", myFunction);
 });
